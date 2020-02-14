e59559d33c2c8a908d9f26a802ef0b332f7a9214
Bryan Soltis <bryan@soltisweb.com>
2020-02-14 08:46:36 -0600

    Add files via upload

---
+++ LicenseRecord.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContosoLicenseFunctions
+{
+    public class LicenseRecord
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "platenumber")]
+        public string PlateNumber { get; set; }
+
+        [JsonProperty(PropertyName = "location")]
+        public string Location { get; set; }
+
+        [JsonProperty(PropertyName = "datecaptured")]
+        public DateTime DateCaptured { get; set; }
+
+        [JsonProperty(PropertyName = "photourl")]
+        public string PhotoURL { get; set; }
+
+        [JsonProperty(PropertyName = "isprocessed")]
+        public bool IsProcessed { get; set; }
+
+        [JsonProperty(PropertyName = "dateprocessed", NullValueHandling = NullValueHandling.Ignore)]
+        public DateTime DateProcessed { get; set; }
+    }
+}
+
